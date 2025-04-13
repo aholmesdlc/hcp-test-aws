3fe2b7b0d8c052040281745c981d78f9ee481783
aholmesdlc <aholmes@Asantes-MacBook-Pro.local>
2025-04-13 09:42:52 -0700

    add port & protocol to listener

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
-terraform { 
-  cloud { 
-    
-    organization = "DLC-HCP" 
+terraform {
+  cloud {
 
-    workspaces { 
-      name = "hcp-workspace-aws" 
-    } 
-  } 
-  
+    organization = "DLC-HCP"
+
+    workspaces {
+      name = "hcp-workspace-aws"
+    }
+  }
+
 }
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "terraweb1" {
-  instance_type = "t2.micro"  
-  ami = "ami-04aabd45b36980079"
+  instance_type = "t2.micro"
+  ami           = "ami-04aabd45b36980079"
 }
 resource "aws_instance" "terraweb2" {
-  instance_type = "t2.micro"   
-  ami = "ami-04aabd45b36980079"
+  instance_type = "t2.micro"
+  ami           = "ami-04aabd45b36980079"
 }
 
