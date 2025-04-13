1df9318b43debfaec2cbabd887368c7e977a3b9c
Asante Holmes <aholmes@Asantes-MacBook-Pro.local>
2025-04-12 18:34:50 -0700

    second commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform { 
+  cloud { 
+    
+    organization = "DLC-HCP" 
+
+    workspaces { 
+      name = "hcp-workspace-aws" 
+    } 
+  } 
+  
+}
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TerraformExample"
+  }
+}
