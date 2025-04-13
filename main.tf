8a96a6800f878e18fc32c29d30885820af2d66d8
aholmesdlc <aholmes@Asantes-MacBook-Pro.local>
2025-04-13 12:07:54 -0700

    changed sg ref to actual sg :)

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "terraweb1" {
   instance_type   = "t2.micro"
   ami             = "ami-04aabd45b36980079"
   key_name        = "kp-4-2-25"
-  vpc_security_group_ids      = ["sg-0123456789abcdef0"]
+  vpc_security_group_ids      = ["sg-015a80c5e8b53fa22"]
 
   provisioner "remote-exec" {
     inline = [
@@ -40,7 +40,7 @@ resource "aws_instance" "terraweb2" {
   instance_type   = "t2.micro"
   ami             = "ami-04aabd45b36980079"
   key_name        = "kp-4-2-25"
-  vpc_security_group_ids      = ["sg-0123456789abcdef0"]
+  vpc_security_group_ids      = ["sg-015a80c5e8b53fa22"]
 
   provisioner "remote-exec" {
     inline = [
