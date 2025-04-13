9298bb18e71212620781209ba8eb83abf5dd4099
aholmesdlc <aholmes@Asantes-MacBook-Pro.local>
2025-04-12 22:04:56 -0700

    import leftover Tgrp & add ALB back2

--- main2.tf
+++ main2.tf
@@ -1,11 +1,3 @@
-resource "aws_instance" "example_server" {
-  ami           = "ami-04e914639d0cca79a"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "TerraformExample"
-  }
-}
 resource "aws_lb" "terraweb-lb" {
   name               = "terraweb-lb"
   internal           = false
