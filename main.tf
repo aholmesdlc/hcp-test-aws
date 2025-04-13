bc2144f4edb0e65ff186aaed7471b42e170939b3
aholmesdlc <aholmes@Asantes-MacBook-Pro.local>
2025-04-13 09:58:32 -0700

    add user-data to terraweb1

--- main.tf
+++ main.tf
@@ -16,6 +16,15 @@ provider "aws" {
 resource "aws_instance" "terraweb1" {
   instance_type = "t2.micro"
   ami           = "ami-04aabd45b36980079"
+  user_data     = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+
+              echo "<html><h1>Deployed by Terraform 🚀</h1></html>" > /var/www/html/index.html
+              EOF
 }
 resource "aws_instance" "terraweb2" {
   instance_type = "t2.micro"
