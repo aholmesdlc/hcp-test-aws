fb49f2d8f44ec99844c31a2235158b254be5f25a
aholmesdlc <aholmes@Asantes-MacBook-Pro.local>
2025-04-13 11:49:43 -0700

    re-apply without tg attachment5

--- alb.tf
+++ alb.tf
@@ -16,7 +16,7 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.terraweb-tg.arn
   }
 }
-resource "aws_lb_target_group" "terraweb-t1" {
+resource "aws_lb_target_group" "terraweb-tg" {
   port     = "80"
   protocol = "HTTP"
   vpc_id   = "vpc-06b76541cfa2305ca"
