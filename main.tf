21e5620edaf56d5b526b6f7cba9cc3b0ba83e280
aholmesdlc <aholmes@Asantes-MacBook-Pro.local>
2025-04-12 21:23:19 -0700

    Remove ALB & Listenter2

--- main.tf
+++ main.tf
@@ -21,16 +21,4 @@ resource "aws_instance" "terraweb2" {
   instance_type = "t2.micro"   
   ami = "ami-04aabd45b36980079"
 }
-resource "aws_lb" "terraweb-lb" {
-  name               = "terraweb-lb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = "sg-015a80c5e8b53fa22"
-  subnets            = ["subnet-0de5d0617ce4b24a6","subnet-03cde2875fcc4238b"]
-
-  enable_deletion_protection = false
-}
-resource "aws_lb_listener" "http" {
-  # (resource arguments)
-}
 
