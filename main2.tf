4e255deabfb255452927a4d11f066236a6df9bf4
aholmesdlc <aholmes@Asantes-MacBook-Pro.local>
2025-04-12 22:03:41 -0700

    import leftover Tgrp & add ALB back

---
+++ main2.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "example_server" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TerraformExample"
+  }
+}
+resource "aws_lb" "terraweb-lb" {
+  name               = "terraweb-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["sg-015a80c5e8b53fa22"]
+  subnets            = ["subnet-0de5d0617ce4b24a6","subnet-03cde2875fcc4238b"]
+
+  enable_deletion_protection = false
+}
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.terraweb-lb
+  default_action {
+   type = "forward"
+   target_group_arn = aws_lb_target_group.terraweb-tg.arn
+   }
+  }
+resource "aws_lb_target_group" "terraweb-tg" {
+}
+
