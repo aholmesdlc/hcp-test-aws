c8bda253a42832ec2c05e6b8feb8cd1fffb7d3d4
aholmesdlc <aholmes@Asantes-MacBook-Pro.local>
2025-04-13 18:45:52 -0700

    create ec2-removed-tf-variable

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "terraweb1" {
   key_name               = "kp-4-2-25"
   vpc_security_group_ids = ["sg-015a80c5e8b53fa22"]
   subnet_id              = "subnet-0de5d0617ce4b24a6"
-
+}
 #   provisioner "remote-exec" {
 #     inline = [
 #       "sudo yum update -y",
